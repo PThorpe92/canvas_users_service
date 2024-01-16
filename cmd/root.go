cd3561421262986f9f0d1cb9406fb28fa121bcb0
PThorpe92 <preston@unlockedlabs.org>
2024-01-16 14:03:46 -0500

    feat: fix compile errors and db init

--- cmd/root.go
+++ cmd/root.go
@@ -3,58 +3,48 @@ package cmd
 import (
 	"fmt"
 	"os"
-  "net/http"
-  "encoding/json"
+
 	dotenv "github.com/joho/godotenv"
 )
 
 type App struct {
+	UnlockedUrl string `json:"unlocked_url"`
 
-UnlockedUrl string `json:"unlocked_url"`
+	UnlockedKey string `json:"unlocked_key"`
 
-UnlockedKey string `json:"unlocked_key"`
+	CanvasUrl string `json:"canvas_url"`
 
-CanvasUrl string `json:"canvas_url"`
-
-CanvasKey string `json:"canvas_key"`
-
+	CanvasKey string `json:"canvas_key"`
 }
 
-var service = http.HandleFunc("/userservice", func(w http.ResponseWriter, r *http.Request) {
-  var body, err = ioutil.ReadAll(r.Body)
-  if err != nil {
-    fmt.Println("Error reading body")
-  }
-  var app App
-  app = json.Unmarshal(body, )
-  fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-})
-
-
 func InitApp(unlocked_url string, unlocked_key string, canvas_url string, canvas_key string) *App {
-  if os.Stat(".env") == nil {
-    err := dotenv.Load()
-    if err != nil {
-  fmt.Println("Error loading .env file")
-    }
+	_, err := os.Stat(".env")
+	if err == nil {
+		err := dotenv.Load()
+		if err != nil {
+			fmt.Println("Error loading .env file")
+		}
+	} else {
+		fmt.Println("No .env file found")
+	}
 
-  if unlocked_url == "" {
-   unlocked_url = os.Getenv("UNLOCKED_URL") 
-  }
-  if unlocked_key == "" {
-    unlocked_key = os.Getenv("UNLOCKED_KEY") 
-  }
-  if canvas_url == "" {
-    canvas_url = os.Getenv("CANVAS_URL") 
-  }
-  if canvas_key == "" {
-    canvas_key = os.Getenv("CANVAS_KEY") 
-  }
-  var app App = App{
-    UnlockedUrl: unlocked_url,
-    UnlockedKey: unlocked_key,
-    CanvasUrl: canvas_url,
-    CanvasKey: canvas_key,
-  }
-  return &app
+	if unlocked_url == "" {
+		unlocked_url = os.Getenv("UNLOCKED_URL")
+	}
+	if unlocked_key == "" {
+		unlocked_key = os.Getenv("UNLOCKED_KEY")
+	}
+	if canvas_url == "" {
+		canvas_url = os.Getenv("CANVAS_URL")
+	}
+	if canvas_key == "" {
+		canvas_key = os.Getenv("CANVAS_KEY")
+	}
+	var app App = App{
+		UnlockedUrl: unlocked_url,
+		UnlockedKey: unlocked_key,
+		CanvasUrl:   canvas_url,
+		CanvasKey:   canvas_key,
+	}
+	return &app
 }
--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
-/*            API:
-* Import Canvas Users to UnlockEd
-* canvasservice import -u <url> -k <api_key>
-*
-* Match Canvas Users to UnlockEd Users
-* canvasservice match -u <url> -k <api_key>
-*
-* OR: ENV VARS
-*
-* CANVAS_URL
-* UNLOCKED_URL
-*
- */
-
-type Args struct {
-	BaseUrl string
-	Key     string
-	Import  bool
-	Match   bool
-}
+import (
+	"canvas_users_service/cmd"
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	db := cmd.Init()
+	defer db.Close()
+
+	// TODO: Register the routes and outline + define the API, separate RPC's in the URI path
+	http.HandleFunc("/user_service", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `
+        Global Params:
+
+        CANVAS_URL and CANVAS_KEY can either be loaded from .env file, or passed in body of request as JSON.
+
+        UNLOCKED_URL and UNLOCKED_KEY can either be loaded from .env file, or passed in body of request as JSON.
+
+        "Export all users: POST /user_service/export_users" (returns an array of CanvasUsers)
+
+        "Import users: POST /user_service/import_users" (returns an array of UnlockedUsers and an array of ProviderUserMapping)
+
+        Params:
+        all: true (imports all users from canvas instance to Unlocked) 
+        users: [1, 2, 3] (imports specific users with ID's 1, 2, 3)
+        accounts: [1, 2, 3] (imports all users from specific accounts with ID's 1, 2, 3)
+
+        "Match users: POST /user_service/match_users" (returns an array of ProviderUserMapping)
+        Params:
+        all: true (matches all users from canvas instance to Unlocked) 
+        users: [1, 2, 3] (matches specific users with ID's 1, 2, 3)
+        accounts: [1, 2, 3] (matches all users from specific accounts with ID's 1, 2, 3)
+        `)
+	})
+
+	// Start the server on port 8080
+	http.ListenAndServe(":8090", nil)
 }
